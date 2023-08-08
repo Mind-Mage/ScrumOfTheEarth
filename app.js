85bb3f299a64f4be95e4347d795becd3e5151ac1
Amanda Hezekiah <amanda.hezekiah@gmail.com>
2023-08-08 15:34:38 -0600

    Home page and some front end

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
-console.log("Life, is Roblox")
+const express = require('express');
+const app = express();
+
+
+const port = 9000;
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+app.use(express.static('public'));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+
+app.get('/charaters', (req, res) => {
+    res.send('Hello World!');
+});
+
+
+app.get('/login', (req, res) => {
+    res.send('Hello World!');
+});
+
+
+app.get('/register', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log("Express listening on port", port);
+});
