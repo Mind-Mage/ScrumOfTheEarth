e031bf054ff5e3f1e15086077c2c401363441cf3
TOONZK <126704941+LeighVirrey@users.noreply.github.com>
2023-08-09 14:48:29 -0600

    connections
    connects pages to the front end

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bcrypt = require('bcrypt')
 const port = 3000
 app = express()
 app.set("view engine", "pug")
+//app.set("view engine", "ejs")
 app.use(express.static('public'))
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
@@ -13,7 +14,7 @@ app.use(session(sessionConfig))
 app.use(cookieParser())
 
 app.get('/', (req,res) =>{
-    res.render('home.html')
+    res.render('home')
 })
 
 app.get('/login', (req,res) =>{
@@ -21,6 +22,9 @@ app.get('/login', (req,res) =>{
 })
 app.get('/register', (req,res) =>{
     res.render('register')
+})
+app.get('/character', (req,res) =>{
+    res.render('createChar')
 })
 
 app.listen(port, () => {
